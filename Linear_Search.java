d0a7f46534eb67a3c0be275fbafb9e9f56b55479
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-04-20 11:27:58 +0530

    Linear Search

---
+++ Linear_Search.java
@@ -0,0 +1,16 @@
+
+public class Linear_Search {
+//
+	public static void main(String[] args) {
+		int[] marks = {90,45,65,72,100,54};
+		int key = 72; 
+		for(int i=0; i<marks.length;i++)
+		{
+		if(key == marks[i])
+			{
+			System.out.println("yes, key is present at "+ i);
+			break; 
+			}
+		}}}
+
+
